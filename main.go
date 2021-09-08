151139058d32970fd78fef190cdecee5e7e18eac
Peter Kay <contact@kaypeter.com>
2021-09-07 23:05:47 -0400

    add create dotfile function

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
     "fmt"
+    "os"
 ) 
 
 func main() {
     fmt.Println("I am alive!")
+}
+
+func createDotfile(path string) *os.File {
+    fmt.Println("Creating dotfile...")
+    f, err := os.Create(path)
+    if err != nil {
+        panic(err)
+    }
+    return f
 }
